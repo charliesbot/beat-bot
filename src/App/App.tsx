3a75787e9c05fb4cdaf88b8f416a6a3106997f66
Charlie <charlie@wizeline.com>
2019-01-14 11:47:55 -0600

    add switch

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { Container } from "./App.styled";
 import Login from "../Login";
 import TopTracks from "../TopTracks";
@@ -7,8 +7,10 @@ import TopTracks from "../TopTracks";
 const App = () => {
   return (
     <Container>
-      <Route path="/login" component={Login} />
-      <Route exact path="/" component={TopTracks} />
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route exact path="/" component={TopTracks} />
+      </Switch>
     </Container>
   );
 };
