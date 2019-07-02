24b62f38b29006c3810b801f6cb90d85ebc0429f
Charlie <charliesbot@gmail.com>
2019-07-01 19:36:41 -0500

    setup nextjs

---
+++ app/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Link from "next/link";
+import { NextPage } from "next";
+import Layout from "../components/Layout";
+
+const IndexPage: NextPage = () => {
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>Hello Next.js 👋</h1>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+    </Layout>
+  );
+};
+
+export default IndexPage;
